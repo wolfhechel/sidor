bebe2fdb5d5819e88df9bd07cc73c6f4a6ad39d9
Pontus Jensen Karlsson <pontus@jensenkarlsson.se>
2024-07-01 14:32:15 +0200

    Missed a component

---
+++ src/lib/components/CollapsedLayout.svelte
@@ -0,0 +1,165 @@
+<div id="wrapper" class="no-scrollbar">
+    <main>
+        <slot name="main"></slot>
+    </main>
+
+    <div id="navigation">
+        <nav class="no-scrollbar">
+            <slot name="nav" />
+        </nav>
+        <aside>
+            <input type="checkbox" id={`toggle-context`} />
+            <label for={`toggle-context`}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </label>
+            <div>
+                <slot name="aside" />
+            </div>
+        </aside>
+    </div>
+</div>
+
+<style lang="scss">
+    $desktop: 1300px;
+
+    .no-scrollbar {
+        -ms-overflow-style: none;
+        scrollbar-width: none;
+
+        &::-webkit-scrollbar {
+            display: none;
+        }
+    }
+
+    div#wrapper {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+
+    div#navigation {
+        background-color: white;
+        border-top: 1px solid var(--border-color);
+        display: flex;
+        gap: 5px;
+        z-index: 2;
+        container: navigation-bar / size;
+        height: 40px;
+    }
+
+    main {
+        overflow: hidden;
+        flex: 3;
+    }
+
+    nav {
+        flex: 1;
+        display: flex;
+        overflow-x: scroll;
+    }
+
+    aside {
+        height: 100%;
+        aspect-ratio: 1 / 1;
+
+        input {
+            display: none;
+        }
+
+        div {
+            display: none;
+            background-color: var(--background-color);
+        }
+
+        label {
+            display: flex;
+            height: 100%;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            gap: 5px;
+            padding: 0 10px;
+
+            span {
+                border-top: 1px solid var(--text-color);
+                border-bottom: 1px solid var(--text-color);
+                width: 20px;
+
+                transform-origin: left center;
+                transform: rotate(0deg);
+                transition: 0.2s ease-in-out;
+            }
+        }
+
+        input[type="checkbox"]:checked ~ {
+            & label {
+                background-color: var(--button-hover-color);
+
+                span {
+                    &:nth-child(1) {
+                        transform: translate(3px, 0) rotate(45deg);
+                    }
+
+                    &:nth-child(2) {
+                        width: 0%;
+                        opacity: 0;
+                    }
+
+                    &:nth-child(3) {
+                        transform: translate(3px, 0) rotate(-45deg);
+                    }
+                }
+            }
+        }
+    }
+
+    @container navigation-bar (orientation: landscape) {
+        input[type="checkbox"]:checked ~ div {
+            display: block;
+            position: absolute;
+            bottom: 100%;
+            left: 0;
+            width: 100%;
+        }
+    }
+
+    @media (orientation: landscape) {
+        div#wrapper {
+            flex-direction: row;
+            width: 100%;
+            max-width: unset;
+        }
+
+        div#navigation {
+            display: contents;
+        }
+
+        main {
+            width: 100%;
+            order: 1;
+        }
+
+        nav {
+            width: auto;
+            order: 2;
+        }
+
+        aside {
+            display: flex;
+            justify-content: end;
+            order: 0;
+            flex: 1;
+            width: auto;
+        }
+
+        aside div {
+            display: initial;
+        }
+
+        aside label {
+            display: none;
+        }
+    }
+</style>
