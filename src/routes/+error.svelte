c4f5fc11dc2dab24e16389a9c2d6eab1ecc1f4d6
Pontus Jensen Karlsson <pontus@jensenkarlsson.se>
2024-07-14 09:25:54 +0200

    Redirecting 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { goto } from "$app/navigation";
+    import { page } from "$app/stores";
+
+    $: $page.status == 404 && goto("/");
+</script>
+
+<div class="full max-w-md m-auto flex items-center justify-center flex-col">
+    <h1 class="text-xl text-slate-800">{$page.status}</h1>
+    <p class="text-slate-600">{$page.error?.message}</p>
+</div>
