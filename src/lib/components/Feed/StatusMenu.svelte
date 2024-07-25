dd18495b9d5648d4726a29eb819978dd01577122
Pontus Jensen Karlsson <pontus@jensenkarlsson.se>
2024-07-26 00:09:23 +0200

    Fix borders

--- src/lib/components/Feed/StatusMenu.svelte
+++ src/lib/components/Feed/StatusMenu.svelte
@@ -12,7 +12,7 @@
     {#each options as [status, label]}
         <li class="flex-1">
             <label
-                class="block whitespace-nowrap no-underline text-center p-2 border-b-2 border-transparent box-content hover:bg-hover has-[:checked]:border-accent"
+                class="cursor-pointer block whitespace-nowrap no-underline text-center p-2 border-b-2 border-transparent box-content hover:bg-hover border-gray-200 has-[:checked]:border-accent has-[:checked]:font-semibold"
             >
                 <input
                     type="radio"
