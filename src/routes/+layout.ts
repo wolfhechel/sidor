722001c9b712836965583058a1c4079422f041c2
Pontus Jensen Karlsson <pontus@jensenkarlsson.se>
2024-08-24 16:46:55 +0200

    Disable global prerendering and re-enable SSP

--- src/routes/+layout.ts
+++
@@ -1,7 +0,0 @@
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
-export const prerender = true;
-
-// since the app is run statically in the browser we don't want 
-// side-effects from server side rendering during development.
-export const ssr = false;
